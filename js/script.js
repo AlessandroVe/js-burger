c089d27244708bfe183835dd1d31c2a2eebac820
AlessandroVe <aveneziano536@gmail.com>
2021-06-07 15:25:54 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-var createBurger = document.getElementsByClassName('add-items')[0];
+var createsum = document.getElementsById('create-burger');
+
+var resultSumElement = document.getElementsById('burger-cost');
+
+createsum.addEventListener("click", function(){
+   var resultSumInt = 0;
+
+   var checkNum = document.getElementsByClassName("add-items");
+
+   for(var i=o; i < checkNum.length; i++ ){
+       if(checkNum[i].checked){ 
+
+           resultSumInt += parseInt(checkNum[i].value);
+
+       }
+   }
+    resultSumElement.innerHTML = " il risultato e' " + resultSumInt;
+})
