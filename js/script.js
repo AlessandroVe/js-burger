7e0db56a2a20e6c25485e58a7eb030b5d8adeafa
AlessandroVe <aveneziano536@gmail.com>
2021-06-08 12:00:38 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+var user_choose = prompt("Che panino preferisci?");
+document.getElementById("title-burger").innerHTML= user_choose;
+if(user_choose.length===0){
+    alert("Inserisci nome valido");
+}
+
+
+
+
+
 var createsum = document.getElementById('create-burger');
+
 
 var resultSumElement = document.getElementById('burger-cost');
 
@@ -14,12 +25,13 @@ createsum.addEventListener("click", function(){
 
        }
     }
-    resultSumElement.innerHTML = " il risultato e' " + resultSumInt;
+    resultSumElement.innerHTML = resultSumInt + "$";
+
+    var discount = parseInt(document.getElementById('discount').value);
+    var sale= 0.2;
+    if(discount===123456){
+        var totalcost = resultSumInt - (resultSumInt*sale);
+        resultSumElement.innerHTML = totalcost + "$";  
+    }
 })
 
-var discount = parseInt(document.getElementById('discount'));
-var sale= 0.2;
-if(discount===123456){
-    var totalcost = resultSumElement*sale;
-    
-}
